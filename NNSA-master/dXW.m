ab96ccbf6dc419bff63fab67091003268def82a7
syousefi <safoora.yousefi1192@gmail.com>
2015-08-18 14:51:24 -0400

    fixed a bug, improved performance in bp

--- NNSA-master/dXW.m
+++
@@ -1,4 +0,0 @@
-function [ tmp ] = dXW(nn, dXX, i, l1, p, q)
-%DXW calculates dXred/dw{l1}(p, q)      
-    tmp = dXX{l1+1}(:, q + 1) * dSigm(nn.a{l1}(i, :) * nn.W{l1}(:, q)) * nn.a{l1}(i, p);   
-end
